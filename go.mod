6c711440d5dc3e04a9f260c40e95aef61c89595f
Marcelo Almiron <marcegabal@gmail.com>
2024-07-01 17:11:27 -0300

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marcegabal/godesde0
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/marcegabal/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
