c51edd7fbc9dea89b0f6659fc1e1a88b230ec64d
Marcelo Almiron <marcegabal@gmail.com>
2024-07-02 10:28:41 -0300

    Commit 2

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/marcegabal/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoATexto(1558)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
